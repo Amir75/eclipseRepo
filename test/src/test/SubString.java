bf7e91c02adea13276579992b660c1b84d71bd22
Amir75 <baigamir786@gmail.com>
2022-11-30 18:37:26 +0530

    Update SubString.java

--- test/src/test/SubString.java
+++ test/src/test/SubString.java
@@ -4,7 +4,7 @@ public class SubString {
 
 	public static void main(String[] args) {
 		String str="JAKAR20210824160413902";
-		System.out.println(str.substring(10,str.length()).length());
+		System.out.println(str.substring(10,str.length()));
 		
 	}
 }
