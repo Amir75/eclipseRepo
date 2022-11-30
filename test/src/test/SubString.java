9e8dc48a4fd8511438a5c14f2ea3fea31b04d6e1
Amir75 <baigamir786@gmail.com>
2022-11-30 18:39:58 +0530

    comit

--- test/src/test/SubString.java
+++ test/src/test/SubString.java
@@ -5,6 +5,6 @@ public class SubString {
 	public static void main(String[] args) {
 		String str="JAKAR20210824160413902";
 		System.out.println(str.substring(10,str.length()));
-		
+		System.out.println("ok");
 	}
 }
